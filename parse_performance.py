5610b196980bf68ce207bc23301161dbc06c7f4c
ngopal <nikhilgopal@gmail.com>
2019-04-14 19:06:40 -0700

    added parser

--- parse_performance.py
+++ parse_performance.py
@@ -4,7 +4,15 @@ REPORT_DIR = "./reports"
 files = os.listdir(REPORT_DIR)
 files = [f for f in files if ".txt" in f]
 
+output = open('portfolio_data.csv', "w")
+
 for f in files:
     fo = open(REPORT_DIR+os.sep+f, "r")
-    print(f, [l for l in fo.readlines()])
+    ticker = f.split(".")[0].strip()
+    lines = [l for l in fo.readlines()]
+    val_loss = lines[1].split(" ")[1].strip()
+    slope = lines[-1].split(" ")[1].strip()
+    print(ticker, val_loss, slope)
+    output.write(ticker+','+val_loss+','+slope+'\n')
     fo.close()
+output.close()
