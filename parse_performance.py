a7a8e6eebd5d385651a830344c1e981aec3b519a
ngopal <nikhilgopal@gmail.com>
2019-04-17 18:45:03 -0700

    prepush

--- parse_performance.py
+++ parse_performance.py
@@ -16,3 +16,18 @@ for f in files:
     output.write(ticker+','+val_loss+','+slope+'\n')
     fo.close()
 output.close()
+
+files = [f for f in files if "MAE.txt" in f]
+
+output = open('portfolio_data.csv', "w")
+
+for f in files:
+    fo = open(REPORT_DIR+os.sep+f, "r")
+    ticker = f.split(".")[0].strip()
+    lines = [l for l in fo.readlines()]
+    val_loss = lines[1].split(" ")[1].strip()
+    slope = lines[-1].split(" ")[1].strip()
+    print(ticker, val_loss, slope)
+    output.write(ticker+','+val_loss+','+slope+'\n')
+    fo.close()
+output.close()
