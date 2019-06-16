8746af9a61c030c697640cf57c1c705b12b8dd3b
ngopal <nikhilgopal@gmail.com>
2019-06-15 20:24:20 -0700

    same medusa

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-sh run.sh $(cat tickers.txt)
+echo "Starting Script"
+Rscript ./data/generators/X.Multiple.Rscript $(cat tickers/tickers_all.txt)
+wait;
+echo "Generating Features and Building Model"
+python 
